f3f061275b8856373c3a254f10a50bf4d7b07a04
xetadeveloper <jazelogin@hotmail.com>
2023-01-23 20:31:33 +0100

    make port number come from process.env

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
 async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
-  });
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => {
+            return {
+                trackAPI: new TrackAPI(),
+            };
+        },
+    });
 
-  const { url, port } = await server.listen();
-  console.log(`
+    const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
+    console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
       📭  Query at ${url}
